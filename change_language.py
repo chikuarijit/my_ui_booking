713e7853846626c9573e80e34c29dbcb0ebed033
chikuarijit <chikuarijitdas@gmail.com>
2023-07-30 18:08:59 +0530

    Third Commit

--- change_language.py
+++ change_language.py
@@ -1,9 +1,9 @@
-from . import utils
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import slash
 import time
+from . import utils
+from . import constants as const
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ChangeLanguage:
@@ -12,9 +12,11 @@ class ChangeLanguage:
 
     def launch(self):
         if not self.loaded():
-            wait = utils.get_wait(self.driver)
-            button = wait.until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="header-language-picker-trigger"]')))
+            slash.logger.info("Launching Language Page")
+            button = utils.get_wait(self.driver).until(
+                EC.element_to_be_clickable
+                ((By.CSS_SELECTOR, const.LANGUAGE_BUTTON)))
+            slash.logger.info("Clicking on Language Button")
             button.click()
             slash.logger.info("Language Page launched")
         else:
@@ -23,10 +25,39 @@ class ChangeLanguage:
 
     def loaded(self):
         try:
-            wait = untils.get_wait(self.driver)
-            language_heading = wait.until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, 'h2.fcab3ed991.f0d4d6a2f5.fda3b74d0d'))
-            )
-            return language_heading.text.strip() == "Select your currency"
+            language_heading = utils.get_wait(self.driver).until(
+                EC.visibility_of_element_located
+                ((By.CSS_SELECTOR, const.LANGUAGE_PAGE)))
+            slash.logger.info("Language Page loaded")
+            return language_heading.text.\
+                strip() == const.LANGUAGE_TEXT
         except:
             return False
+
+    def select_language(self, language_code):
+        if self.loaded():
+            try:
+                slash.logger.info("Selecting Language")
+                language_list = utils.get_wait(self.driver).until(
+                    EC.presence_of_all_elements_located
+                    ((By.CSS_SELECTOR, const.SELECT_LANGUAGE)))
+
+                for language in language_list:
+                    if language_code in language.text:
+                        slash.logger.info(
+                            f"Selecting {language_code}")
+                        language.click()
+                        slash.logger.info(
+                            f"Language "
+                            f"'{language_code}' selected.")
+                        break
+            except:
+                slash.logger.info(
+                    f"Failed to "
+                    f"select currency "
+                    f"'{language_code}'.")
+        else:
+            slash.logger.info(
+                "Language Page is not "
+                "loaded yet. Call 'launch()' "
+                "method first.")
